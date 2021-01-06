39371e7793e765eb954b2cb08d57254e3d41e01e
leahcimsu <leahcimsu@tencent.com>
2021-01-06 16:27:19 +0800

    feat: start

---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-06 16:26:41
+ * @LastEditTime: 2021-01-06 16:27:02
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /fake-gin/main.go
+ */
+package main
+
+func main() {
+
+}
